7ec2582244b9c3f922f4916bd51be4ec422d0f24
Robin Cabeza Ruiz <robbinc91@gmail.com>
2021-05-05 11:13:15 -0400

    adding exhaustive registration to preprocessing stage

--- hammers_2017_preprocessing.py
+++ hammers_2017_preprocessing.py
@@ -4,12 +4,19 @@ import SimpleITK as sitk
 
 
 def do_preprocess(root):
-    for i in range(12, 31):
+    for i in range(1, 31):
         _name = 'a{:02d}'.format(i)
-        corrected_image, bias_field, log_bias_field = n4_bias_field_correction(root + 'images/' + _name + '.nii.gz')
-        sitk.WriteImage(corrected_image, root + 'pre/' + _name + '-bfc.nii.gz')
+        # Bias field Correction
+        #corrected_image, bias_field, log_bias_field = n4_bias_field_correction(root + 'images/' + _name + '.nii.gz')
+        #sitk.WriteImage(corrected_image, root + 'pre/' + _name + '-bfc.nii.gz')
         # sitk.WriteImage(bias_field, root + 'pre/' + _name + '-bf.nii.gz')
         # sitk.WriteImage(log_bias_field, root + 'pre/' + _name + '-lbf.nii.gz')
+
+        # Image Registration
+        transform, resampled = exhautive_registration(IMG_ROOT + 'registration/MNI152_T1_1mm.nii.gz', root + 'pre/' + _name + '-bfc.nii.gz')
+        sitk.WriteTransform(transform, root + 'pre/' + _name + '-transform.tfm')
+        sitk.WriteImage(resampled, root + 'pre/' + _name + '-reg.nii.gz')
+
 
 
 if __name__ == '__main__':
